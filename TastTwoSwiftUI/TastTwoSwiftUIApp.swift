873fc3c5a7cd8035dd316b7dd4c1bcb2c3f587c1
litvinovDanil112011 <danil112011@mail.ru>
2022-03-25 14:00:18 +0300

    Initial Commit

---
+++ TastTwoSwiftUI/TastTwoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TastTwoSwiftUIApp.swift
+//  TastTwoSwiftUI
+//
+//  Created by Daniil Litvinov on 25.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct TastTwoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
